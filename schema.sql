29cdca71c3544428b38fa69612d7e8f39fac6ff8
Fabien <fabienswift@gmail.com>
2022-08-10 19:32:47 -0400

    Update schema,data and queries files

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 
 
 CREATE TABLE animals( 
-    id integer PRIMARY KEY,
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name text, 
     date_of_birth date, 
     escape_attempts integer, 
@@ -10,3 +10,22 @@ CREATE TABLE animals(
     weight_kg decimal,
     species text
 );
+
+CREATE TABLE owners (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name text, 
+    age integer
+);
+
+CREATE TABLE species (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name text
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id integer;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD owner_id integer;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
